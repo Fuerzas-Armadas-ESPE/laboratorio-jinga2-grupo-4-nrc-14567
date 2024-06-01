8b594da75e8bd0baeff13954475a36c7fc724558
Javier Gonzaga <jcgonzaga2@espe.edu.ec>
2024-06-01 14:41:45 -0500

    Cambios en los archivos para los cruds

--- productos/urls.py
+++ productos/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # URLs de vistas normales
-    # Crear la URL de la vista index
+    path('', views.listar_productos, name='listar_productos'),
+    path('producto/<int:pk>/', views.detalle_producto, name='detalle_producto'),
+    path('producto/nuevo/', views.crear_producto, name='crear_producto'),
+    path('producto/<int:pk>/editar/', views.actualizar_producto, name='actualizar_producto'),
+    path('producto/<int:pk>/eliminar/', views.eliminar_producto, name='eliminar_producto'),
 ]
