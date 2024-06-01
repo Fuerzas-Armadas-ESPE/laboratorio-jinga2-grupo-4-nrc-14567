20de5086ca0cf37c84a6de201b3a1d04becc1e76
Javier Gonzaga <jcgonzaga2@espe.edu.ec>
2024-06-01 16:44:24 -0500

    cambios

--- inventario/settings.py
+++ inventario/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-)k0^2(x0-4xsls#(2q+$ugp5+5t0vbq40vhk45$&7+xlvmq21&'
+SECRET_KEY = 'django-insecure-y&o6ujes+xdtpephujp0iiwulhcns*&ta7@gm8pex9xf0g^wos'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    # Registrar la app productos
+    'productos',
 ]
 
 MIDDLEWARE = [
--- productos/views.py
+++ productos/views.py
@@ -1,8 +1,39 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Producto
-
-productos = []
+from .forms import ProductoForm  # Asumiendo que tienes un formulario llamado ProductoForm
 
 def listar_productos(request):
-    # Consulta a la base de datos
-    # Renderiza la plantilla listar.html
+    productos = Producto.objects.all()
+    return render(request, 'listar_productos.html', {'productos': productos})
+
+def detalle_producto(request, pk):
+    producto = get_object_or_404(Producto, pk=pk)
+    return render(request, 'detalle_producto.html', {'producto': producto})
+
+def crear_producto(request):
+    if request.method == "POST":
+        form = ProductoForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('listar_productos')
+    else:
+        form = ProductoForm()
+    return render(request, 'crear_producto.html', {'form': form})
+
+def actualizar_producto(request, pk):
+    producto = get_object_or_404(Producto, pk=pk)
+    if request.method == "POST":
+        form = ProductoForm(request.POST, instance=producto)
+        if form.is_valid():
+            form.save()
+            return redirect('listar_productos')
+    else:
+        form = ProductoForm(instance=producto)
+    return render(request, 'actualizar_producto.html', {'form': form})
+
+def eliminar_producto(request, pk):
+    producto = get_object_or_404(Producto, pk=pk)
+    if request.method == "POST":
+        producto.delete()
+        return redirect('listar_productos')
+    return render(request, 'eliminar_producto.html', {'producto': producto})
